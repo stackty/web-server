5ed080584f80c52bb01c1aac5d36bdcb0e29572e
Tynan Stack <tynan.stack@gmail.com>
2016-06-20 08:13:31 -0700

    Initialize reop

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(request, response, next){
+		console.log('private route hit');
+		next();
+},
+	logger: function(request,response,next){
+		console.log('Request:'+request.method+' '+request.originalUrl+ new Date().toString());
+		next()
+	}}
+app.use(middleware.logger);
+
+app.get('/', function(reqest, response){
+	response.send('Hello Express');
+});
+app.get('/about', middleware.requireAuthentication, function(request,response){
+	response.send('about Us');
+})
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(PORT,function (){
+	console.log('express server has started on port %d',PORT);
+});
